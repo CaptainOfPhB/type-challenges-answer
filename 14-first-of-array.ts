2396e3a27ffed0ad3f5abcf8fc992a02f90c29bf
CaptainOfPhB <captaininphw@gmail.com>
2021-06-26 17:19:23 +0800

    done No.14

---
+++ 14-first-of-array.ts
@@ -0,0 +1,12 @@
+type First<T extends any[]> = T extends [infer F, ...any[]] ? F : never;
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+]
