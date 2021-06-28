6ff2fe889ffbda95aa9defc38d62c7bdf45f9f32
CaptainOfPhB <captaininphw@gmail.com>
2021-06-28 10:42:08 +0800

    fix typo

--- 11-tuple-to-object.ts
+++ 11-tuple-to-object.ts
@@ -23,9 +23,9 @@ type error = TupleToObject<[[1, 2], {}]>
 // 使用 typeof 可以将 JS 方便的转换为 TS
 
 const array1 = [1, true, 'abc']; 
-type TypeofArrary1 = typeof array1; // (string | number | boolean)[]
-type Array1EleType = TypeofArrary1[number]; // string | number | boolean
+type TypeofArray1 = typeof array1; // (string | number | boolean)[]
+type Array1EleType = TypeofArray1[number]; // string | number | boolean
 
 const array2 = [1, true, 'abc'] as const; 
-type TypeofArrary2 = typeof array2; // readonly [1, true, 'abc']
+type TypeofArray2 = typeof array2; // readonly [1, true, 'abc']
 type Array2EleType = (typeof array2)[number]; // true | 1 | 'abc'
