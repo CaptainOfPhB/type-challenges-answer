14155c385c5036dcea70fd7af1c3a2fc80f3abd1
CaptainOfPhB <captaininphw@gmail.com>
2021-07-01 10:16:20 +0800

    add test case

--- 999-drop-string.ts
+++ 999-drop-string.ts
@@ -9,8 +9,13 @@ const dropString1 = drop('butter fly!', ' ' as const);
 const dropString2 = drop('    butter fly!        ', ' ' as const);
 const dropString3 = drop(' b u t t e r f l y ! ', ' ' as const);
 
+const dropString4 = drop(' b u t t e r f l y ! ', 'b' as const);
+const dropString5 = drop(' b u t t e r f l y ! ', 't' as const);
+
 type cases = [
   Expect<Equal<typeof dropString1, 'butterfly!'>>,
   Expect<Equal<typeof dropString2, 'butterfly!'>>,
   Expect<Equal<typeof dropString3, 'butterfly!'>>,
+  Expect<Equal<typeof dropString4, '  u t t e r f l y ! '>>,
+  Expect<Equal<typeof dropString5, ' b u   e r f l y ! '>>,
 ]
