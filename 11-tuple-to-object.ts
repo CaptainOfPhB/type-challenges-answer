18a381d3ec59574b096151757afa63e7e4f25dd4
CaptainOfPhB <captaininphw@gmail.com>
2021-07-03 10:36:25 +0800

    remove redundant comment

--- 11-tuple-to-object.ts
+++ 11-tuple-to-object.ts
@@ -14,18 +14,3 @@ type cases = [
 
 // @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
-
-
-/* _____________ 思路 _____________ */
-
-// 一个快速获取数组/元组中元素的方法是：T[number]
-// 注意加了 as const 之后的区别
-// 使用 typeof 可以将 JS 方便的转换为 TS
-
-const array1 = [1, true, 'abc']; 
-type TypeofArray1 = typeof array1; // (string | number | boolean)[]
-type Array1EleType = TypeofArray1[number]; // string | number | boolean
-
-const array2 = [1, true, 'abc'] as const; 
-type TypeofArray2 = typeof array2; // readonly [1, true, 'abc']
-type Array2EleType = (typeof array2)[number]; // true | 1 | 'abc'
--- 4-pick.ts
+++ 4-pick.ts
@@ -27,12 +27,3 @@ interface Expected2 {
   title: string
   completed: boolean
 }
-
-
-/* _____________ 思路 _____________ */
-
-/**
- * 观察测试用例可以发现，第一个泛型 T 为 interface，第二个泛型 K 为联合类型，也是前面 interface 的 key。
- * 那么利用 keyof interface 可以获取到 interface 的所有的 key 这个特性，将 K 改写为 "K extends keyof T"，即表示 K 为 T 的所有 key 的集合。
- * 再利用 [k in K]，k 即为 K 中的每一项（类似 JS 中数组循环中的每一项元素），然后值类型为 T[k] 即可（取 T 中与 k 对应的 key 的类型）。
- */
